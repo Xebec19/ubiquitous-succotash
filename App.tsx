372c00bc5d25dac72fe8e934cacb66cb5507e61a
xebec19 <rohant946@gmail.com>
2023-02-19 16:49:58 +0530

    wip: icons for tabs

--- App.tsx
+++ App.tsx
@@ -5,21 +5,32 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './screens/Home.screen';
 import Maps from './screens/Maps.screen';
 import Profile from './screens/Profile.screen';
-
-const StyledView = styled.View`
-  background-color: papayawhip;
-`;
-
-const StyledText = styled.Text`
-  color: palevioletred;
-`;
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
+
+const screenProps = ({route}) => ({
+  tabBarIcon: ({focused, color, size}) => {
+    let iconName;
+
+    if (route.name == 'Home') {
+      iconName = focused
+        ? 'ios-information-circle'
+        : 'ios-information-circle-outline';
+    } else if (route.name == 'Maps') {
+      iconName = focused ? 'ios-list' : 'ios-list-outline';
+    }
+
+    return <Ionicons name={iconName} size={size} color={color} />;
+  },
+  tabBarActiveTintColor: 'tomato',
+  tabBarInactiveTintColor: 'gray',
+});
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={screenProps}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Maps" component={Maps} />
         <Tab.Screen name="Profile" component={Profile} />
